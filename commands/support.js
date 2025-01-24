5bb351d450aa6e4cf00b2eb640f20c7e54d8ceb0
prifd74 <prifdgamer@gmail.com>
2025-01-24 23:25:21 +0530

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -9,10 +9,10 @@ module.exports = {
     options: [],
     run: async (client, interaction, lang) => {
         try {
-            const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-            const githubLink = "https://github.com/GlaceYT";
-            const replitLink = "https://replit.com/@GlaceYT";
-            const youtubeLink = "https://www.youtube.com/@GlaceYT";
+            const supportServerLink = "https://discord.gg/WgKwtcGHky";
+            const githubLink = "INVALID";
+            const replitLink = "INVALID";
+            const youtubeLink = "INVALID";
 
             const embed = new EmbedBuilder()
                 .setColor('#b300ff')
