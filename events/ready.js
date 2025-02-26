5aa55c8c740a5e5b388dc0296748bbf8e3c27e78
prifd74 <prifdgamer@gmail.com>
2025-02-26 20:28:30 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -43,7 +43,7 @@ module.exports = async (client) => {
         //console.log(`🎵 Now Playing: ${trackName}`);
 
         client.user.setActivity({
-            name: `🎸 ${trackName}`,
+            name: `😘🦖 ${trackName}`,
             type: ActivityType.Playing
         });
     }
