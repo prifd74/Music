973ac454748de8a7abf658e759afe317e5cab1a2
prifd74 <prifdgamer@gmail.com>
2025-03-05 22:00:38 +0530

    Update player.js

--- player.js
+++ player.js
@@ -102,7 +102,7 @@ function initializePlayer(client) {
                 iconURL: musicIcons.playerIcon,
                 url: config.SupportServer
             })
-            .setFooter({ text: `Developed by SSRR | Prime Music v1.2`, iconURL: musicIcons.heartIcon })
+            .setFooter({ text: `Developed by Avinan And Glace`, iconURL: musicIcons.heartIcon })
             .setTimestamp()
             .setDescription(  
                 `- **𝐓𝐢𝐭𝐥𝐞:** [${track.info.title}](${track.info.uri})\n` +
