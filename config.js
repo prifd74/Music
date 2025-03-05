10aa5edcb5f34cdbb6a17a42a89dfe59a4e6f775
prifd74 <prifdgamer@gmail.com>
2025-03-05 22:05:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,23 +3,25 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["796001480406466601", ""], 
+  ownerID: ["1004206704994566164", ""], 
   mongodbUri : "mongodb+srv://RTX:pokemon@cluster0.na7n4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  spotifyClientId : "f71a3da30e254962965ca2a89d6f74b9",
+  spotifyClientSecret : "199a619d22dd4e55a4a2c1a7a3d70e63",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/WdnP5DszsY",
+  SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
-       {
+     {
       name: "XX",
       password: "enteryourcustompass",
       host: "180.188.226.76",
       port:  7019,
       secure: false
-    },
+    }
   ]
 }
