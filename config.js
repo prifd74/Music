dd68381747974f3f45e8dd6fa76d2947658018a1
prifd74 <prifdgamer@gmail.com>
2025-03-13 00:03:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,10 +17,10 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-      name: "XX",
-      password: "enteryourcustompass",
-      host: "180.188.226.76",
-      port:  7019,
+      name: "Avinan",
+      password: "pokemon",
+      host: "new-york-node-1.vortexcloud.xyz",
+      port:  9376,
       secure: false
     }
   ]
